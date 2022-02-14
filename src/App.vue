1512173dea58e12793a3ec19b85796bf3037f1e7
Florian-Ermler <Florian@Ermler.dev>
2022-02-14 21:13:56 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <main class="flex flex-col items-center justify-center text-white">
+        <Suspense> <data-table /> </Suspense>
+    </main>
+</template>
+
+<script lang="ts">
+import DataTable from './components/data-table';
+
+export default {
+    name: 'Home',
+    components: {
+        DataTable,
+    },
+};
+</script>
---
+++ src/components/data-table/DataTable.vue
@@ -0,0 +1,40 @@
+<template>
+    <main>
+        <div class="flex flex-col mt-28 justify-center items-start drop-shadow-2xl border-2 rounded">
+            <div v-for="wine in data.data" :key="wine.id">
+                <data-row
+                    :id="wine.id"
+                    :sweetness="wine.sweetness"
+                    :origin="wine.origin"
+                    :fermentation_container="wine.fermentation_container"
+                    :grape_variety="wine.grape_variety"
+                    :stored_since="wine.stored_since"
+                />
+            </div>
+        </div>
+    </main>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/runtime-core';
+import DataRow from '../data-row';
+
+export default defineComponent({
+    name: 'Home',
+    components: {
+        DataRow,
+    },
+    async setup() {
+        const data = ref();
+        try {
+            const res = await fetch('https://rest.ermler.dev/api/wine/');
+            data.value = await res.json();
+        } catch (e) {
+            console.log(e);
+        }
+        return {
+            data,
+        };
+    },
+});
+</script>
